008b6de7d44b5b01cf632459eb96c2f12d2ba6b6
Kirill <78596284+Kirillkik@users.noreply.github.com>
2021-06-28 19:40:22 +0500

    Добавить работу с базой данных PostgreSQL

---
+++ ABTestRealTestApp.Memory/DataBaseContext.cs
@@ -0,0 +1,18 @@
+﻿using ABTestRealTestApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ABTestRealTestApp.Memory
+{
+    public class DataBaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=ec2-54-155-226-153.eu-west-1.compute.amazonaws.com;Port=5432;Database=d5eq1qihhq1qn5;Username=hzbqqkwbltarhw;Password=fbcc46e0c7fed98fa174d948099426fca5ea79a020b58180b59fbac4d6499f5f; TrustServerCertificate = true; SslMode = Require; ");
+        }
+    }
+}
--- ABTestRealTestApp.Memory/UserRepository.cs
+++ ABTestRealTestApp.Memory/UserRepository.cs
@@ -9,40 +9,48 @@ namespace ABTestRealTestApp.Memory
 {
     public class UserRepository : IUserRepository
     {
-        private List<User> Users = new List<User>()
-        {
-            new User(1, DateTime.Parse("1.1.1999"), DateTime.Parse("1.10.1999")),
-            new User(2, DateTime.Parse("1.1.2000"), DateTime.Parse("1.10.2000")),
-            new User(3, DateTime.Parse("1.1.2001"), DateTime.Parse("1.10.2001")),
-        };
 
         public void AddUser(User user)
         {
-            user.Id = Users.Last().Id + 1;
-            Users.Add(user);
+            using (DataBaseContext db = new DataBaseContext())
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+            }
         }
 
         public void DeleteUser(User user)
         {
-            Users.Remove(user);
+            using (DataBaseContext db = new DataBaseContext())
+            {
+                db.Users.Remove(user);
+                db.SaveChanges();
+            }
         }
 
         public User[] GetAllUsers()
         {
-            return Users.ToArray();
+            using (DataBaseContext db = new DataBaseContext())
+            {
+                return db.Users.ToArray();
+            }
+                
         }
 
         public User GetUserById(int id)
         {
-            return Users.FirstOrDefault(x => x.Id == id);
+            using (DataBaseContext db = new DataBaseContext())
+            {
+                return db.Users.FirstOrDefault(x => x.Id == id);
+            }
         }
 
         public void UpdateUsers(User[] users)
         {
-            Users.Clear();
-            foreach (var user in users)
+            using (DataBaseContext db = new DataBaseContext())
             {
-                Users.Add(user);
+                db.Users.UpdateRange(users);
+                db.SaveChanges();
             }
         }
     }
